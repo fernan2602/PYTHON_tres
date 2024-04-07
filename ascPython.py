cf66a40feda2a480abc734bc5b07d35e6ff4f002
Victor Fernando Masias <72745830@continental.edu.pe>
2024-04-06 21:33:29 -0500

    Commit numero uno

---
+++ ascPython.py
@@ -0,0 +1,24 @@
+x1 = int(input("Ingrese primer valor: "))
+x2 = int(input("Ingrese segundo valor: "))
+x3 = int(input("Ingrese tercer valor: "))
+
+if(x1 > x2 & x2 > x3):
+    num1 = x1
+    num2 = x2
+    if(num1 > num2):
+        print(num1)
+    else:
+        print(num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
